36dab9b662d14d659b45767880414097a70a6876
celil-vural <celilvural.work@gmail.com>
2023-09-26 22:13:35 +0300

    Create basic-forum

---
+++ src/main/java/com/basicforum/BasicForumApplication.java
@@ -0,0 +1,13 @@
+package com.basicforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicForumApplication.class, args);
+    }
+
+}
