705d66a07b99e4665ba8a4a712466981b9e56cb7
celil-vural <celilvural.work@gmail.com>
2023-09-26 22:30:41 +0300

    create model.RegisterRequest

---
+++ src/main/java/com/basicforum/model/RegisterRequest.java
@@ -0,0 +1,18 @@
+package com.basicforum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String password;
+}
