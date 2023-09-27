bd514dcd12b7e5ae44e7b82878d8bf1538f82c72
celil-vural <celilvural.work@gmail.com>
2023-09-27 10:38:32 +0300

    disable csrf

--- src/main/java/com/basicforum/config/SecurityConfiguration.java
+++ src/main/java/com/basicforum/config/SecurityConfiguration.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -20,9 +20,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
-                .csrf(csrf->csrf
-                        .ignoringRequestMatchers ("/api/v1/auth/**")
-                )
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize->
                         authorize
                                 .requestMatchers("/api/v1/auth/**").permitAll()
